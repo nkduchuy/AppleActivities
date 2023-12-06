a9a9e7be90592c48ea20b1e357b195405fc1c852
Huy Nguyen <nkduchuy@yahoo.com.vn>
2023-12-05 21:11:35 -0700

    Implement routing for client app

---
+++ client-app/src/app/router/Routes.tsx
@@ -0,0 +1,22 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../layout/App";
+import HomePage from "../../features/home/HomePage";
+import ActivityDashBoard from "../../features/activities/dashboard/ActivityDashBoard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+
+export const routes: RouteObject[] = [
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {path: 'activities', element: <ActivityDashBoard />},
+            {path: 'activities/:id', element: <ActivityDetails />},
+            {path: 'createActivity', element: <ActivityForm key='create' />},
+            {path: 'manage/:id', element: <ActivityForm key='manage' />}
+        ]
+    }
+]
+
+
+export const router = createBrowserRouter(routes);
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
@@ -21,10 +21,10 @@ export default class ActivityStore {
     loadActivities = async () => {
         try
         {
+            this.setLoadingInitial(true);
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id, activity);
+                this.setActivity(activity);
                 })
             this.setLoadingInitial(false);
         }
@@ -35,25 +35,44 @@ export default class ActivityStore {
         }
     }
 
+    loadActivity = async (id:string) => {
+        // Get activity from memory (activityRegister) first then fetch from API if not available
+        let activity = this.getActivity(id);
+
+        if (activity)
+        {
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else
+        {
+            this.setLoadingInitial(true);
+            try {
+                const activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => {
+                    this.selectedActivity = activity;
+                });
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
-    }
-
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
     }
 
     createActivity = async (activity: Activity) => {
@@ -108,8 +127,6 @@ export default class ActivityStore {
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistry.delete(id);
-                if (this.selectedActivity?.id === id)
-                    this.cancelSelectedActivity();
                 this.loading = false;
             })
         }
